bd79514998c377421cf64cc68c81004ce0ee538e
Lars Christensen <larsch@belunktum.dk>
2024-04-27 13:04:25 +0200

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -76,7 +76,7 @@ mod tests {
     }
 }
 
-fn sockaddr_to_socketaddr(addr: &SockAddr) -> Result<net::SocketAddr, ()> {
+fn sockaddr_to_socketaddr(addr: &SockAddr) -> Result<net::SocketAddr, String> {
     match unsafe { addr.sa.sa_family as i32 } {
         libc::AF_INET => {
             let sockaddr_in: &libc::sockaddr_in = unsafe { &addr.sin };
@@ -103,7 +103,7 @@ fn sockaddr_to_socketaddr(addr: &SockAddr) -> Result<net::SocketAddr, ()> {
             let port = u16::from_be(sockaddr_in6.sin6_port);
             Ok(net::SocketAddr::V6(net::SocketAddrV6::new(ip, port, 0, 0)))
         }
-        _ => Err(()),
+        _ => Err("Unhandled address family".to_owned()),
     }
 }
 
@@ -113,7 +113,7 @@ union SockAddr {
     sin6: libc::sockaddr_in6,
 }
 
-fn socketaddr_to_sockaddr(addr: net::SocketAddr) -> Result<SockAddr, ()> {
+fn socketaddr_to_sockaddr(addr: net::SocketAddr) -> Result<SockAddr, String> {
     match addr {
         net::SocketAddr::V4(v4addr) => {
             let ip = v4addr.ip().octets();
@@ -145,88 +145,100 @@ fn socketaddr_to_sockaddr(addr: net::SocketAddr) -> Result<SockAddr, ()> {
         }
     }
 }
+struct PingProtocol {
+    socket: i32,
+    target: SockAddr,
+    target_sa: net::SocketAddr,
+}
+
+impl PingProtocol {
+    fn new(target_sa: net::SocketAddr) -> Result<Self, Box<dyn std::error::Error>> {
+        let target = socketaddr_to_sockaddr(target_sa)?;
+        let target_family = match target_sa {
+            net::SocketAddr::V4(_) => libc::AF_INET,
+            net::SocketAddr::V6(_) => libc::AF_INET6,
+        };
+        let ipproto = match target_sa {
+            net::SocketAddr::V4(_) => libc::IPPROTO_ICMP,
+            net::SocketAddr::V6(_) => libc::IPPROTO_ICMPV6,
+        };
+        let socket = unsafe { libc::socket(target_family, libc::SOCK_DGRAM, ipproto) };
+        if socket < 0 {
+            return Err(std::io::Error::last_os_error())?;
+        }
+        Ok(Self {
+            socket,
+            target,
+            target_sa,
+        })
+    }
+
+    fn send(&self) -> Result<(), std::io::Error> {
+        let mut icmp_packet: [u8; 192] = [0; 192];
+        let code = match self.target_sa {
+            net::SocketAddr::V4(_) => 0x08,
+            net::SocketAddr::V6(_) => 0x80,
+        };
+        let icmp_prefix: [u8; 16] = [code, 0, 0, 0, 0, 1, 0, 1, 0, 1, 2, 3, 4, 5, 6, 7];
+        icmp_packet[0..16].copy_from_slice(&icmp_prefix);
+
+        let buf = &icmp_packet as *const u8 as *const libc::c_void;
+        let result = unsafe {
+            libc::sendto(
+                self.socket,
+                buf,
+                icmp_packet.len(),
+                0,
+                &self.target.sa,
+                std::mem::size_of::<SockAddr>() as u32,
+            )
+        };
+        if result < 0 {
+            return Err(std::io::Error::last_os_error());
+        }
+        Ok(())
+    }
+
+    fn recv(&self) -> Result<Option<net::SocketAddr>, Box<dyn std::error::Error>> {
+        let mut buf = [0u8; 65536];
+        let buf_ptr = &mut buf as *mut u8 as *mut libc::c_void;
+        let flags = 0;
+        let mut addr: SockAddr = unsafe { std::mem::zeroed() };
+        let mut addr_len = std::mem::size_of::<SockAddr>() as u32;
+        let addrlen_ptr = &mut addr_len as *mut u32;
+        let recvfrom_result = unsafe {
+            libc::recvfrom(
+                self.socket,
+                buf_ptr,
+                buf.len(),
+                flags,
+                &mut addr.sa,
+                addrlen_ptr,
+            )
+        };
+        if recvfrom_result < 0 {
+            return Err(std::io::Error::last_os_error())?;
+        }
+        let addr = sockaddr_to_socketaddr(&addr)?;
+        Ok(Some(addr))
+    }
+}
 
 fn main() {
     let target = env::args().nth(1).unwrap();
     let target = dns_lookup::lookup_host(&target).unwrap();
     let target = target.first().unwrap();
-    // let target = match target {
-    //     std::net::IpAddr::V4(v4addr) => *v4addr,
-    //     std::net::IpAddr::V6(_) => panic!(),
-    // };
-
-    let target_family = match target {
-        net::IpAddr::V4(_) => libc::AF_INET,
-        net::IpAddr::V6(_) => libc::AF_INET6,
-    };
-
-    let ipproto = match target {
-        net::IpAddr::V4(_) => libc::IPPROTO_ICMP,
-        net::IpAddr::V6(_) => libc::IPPROTO_ICMPV6,
-    };
-
     let target_sa = match target {
         net::IpAddr::V4(v4addr) => net::SocketAddr::V4(net::SocketAddrV4::new(*v4addr, 58)),
         net::IpAddr::V6(v6addr) => net::SocketAddr::V6(net::SocketAddrV6::new(*v6addr, 58, 0, 0)),
     };
 
-    let target = socketaddr_to_sockaddr(target_sa).unwrap();
+    let ping_protocol = PingProtocol::new(target_sa).unwrap();
 
-    unsafe {
-        let socket = libc::socket(target_family, libc::SOCK_DGRAM, ipproto);
-        if socket < 0 {
-            println!("{:?}", std::io::Error::last_os_error());
-            panic!();
-        }
+    ping_protocol.send().unwrap();
 
-        let mut icmp_packet: [u8; 192] = mem::zeroed();
+    let response = ping_protocol.recv().unwrap();
+    println!("response from {:?}", response);
 
-        let code = match target_sa {
-            net::SocketAddr::V4(_) => 0x08,
-            net::SocketAddr::V6(_) => 0x80,
-        };
-
-        let icmp_prefix: [u8; 16] = [code, 0, 0, 0, 0, 1, 0, 1, 0, 1, 2, 3, 4, 5, 6, 7];
-        icmp_packet[0..16].copy_from_slice(&icmp_prefix);
-
-        println!("{:?}", target_sa);
-
-        let buf = &icmp_packet as *const u8 as *const libc::c_void;
-        let result = libc::sendto(
-            socket,
-            buf,
-            icmp_packet.len(),
-            0,
-            &target.sa,
-            std::mem::size_of::<SockAddr>() as u32,
-        );
-        if result < 0 {
-            println!("sendto failed: {:?}", std::io::Error::last_os_error());
-            panic!();
-        }
-
-        let mut buf = [0u8; 1500];
-        let buf_ptr = &mut buf as *mut u8 as *mut libc::c_void;
-        let len = buf.len();
-        let flags = 0;
-        let mut addr: SockAddr = std::mem::zeroed();
-        let mut addr_len = std::mem::size_of::<SockAddr>() as u32;
-        let addrlen_ptr = &mut addr_len as *mut u32;
-
-        let recvfrom_result =
-            libc::recvfrom(socket, buf_ptr, len, flags, &mut addr.sa, addrlen_ptr);
-        println!("recvfrom_result = {:?}", recvfrom_result);
-
-        let addr = sockaddr_to_socketaddr(&addr);
-        println!("response from {:?}", addr);
-
-        let mut sockname: SockAddr = mem::zeroed();
-        let mut sockname_len = std::mem::size_of::<libc::sockaddr>() as u32;
-        if libc::getsockname(socket, &mut sockname.sa, &mut sockname_len) != 0 {
-            println!("getsockname: {:?}", std::io::Error::last_os_error());
-        }
-
-        println!("sockname_len: {}", sockname_len);
-        println!("sockname: {:?}", sockaddr_to_socketaddr(&sockname));
-    }
+    // Rest of the code...
 }
